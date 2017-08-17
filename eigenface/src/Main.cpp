472618ecfd45fed017d5fcf98d5b75f2d8c802de
miguel <miguel@miguel-Desktop>
2017-08-17 01:46:06 -0500

    second commit

--- eigenface/src/Main.cpp
+++ eigenface/src/Main.cpp
@@ -3,7 +3,7 @@ using namespace cv;
 
 int main(int argc, char** argv) {
 	//second commit
-	Mat inputImage = imread("/home/miguel/test2.png");
+	Mat inputImage = imread("/home/miguel/test.png");
 	imshow("Input Image", inputImage);
 	
 	waitKey(0);
